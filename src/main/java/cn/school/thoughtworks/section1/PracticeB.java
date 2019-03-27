e6d324856fd6cd7a14a9b30b78bd31c467122618
wenjing.qi <gabralia@qq.com>
2019-03-28 00:08:42 +0800

    sublist same elements

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,13 @@
 package cn.school.thoughtworks.section1;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        return collection1.stream()
+                .filter(t-> collection2.stream().anyMatch(sub-> sub.contains(t)))
+                .collect(Collectors.toList());
     }
 }

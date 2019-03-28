a00352e94f0e288fb898b1b2ba934e9ba1638331
wejing.qi <wenjing.qi@thoughtworks.com>
2019-03-28 18:39:35 +0800

    same elements times with '-'

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,27 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String,Integer> map = new HashMap<>();
+        for(int i=0;i<collection1.size();i++){
+            String str = collection1.get(i);
+            Integer times = 1;
+            Integer end =  str.length();
+            if(str.indexOf('-') > 0){
+               end = str.indexOf('-');
+               times = Integer.parseInt(collection1.get(i).substring(end+1));
+            }
+            String subStr = collection1.get(i).substring(0,end);
+            if(!map.containsKey(subStr)){
+                map.put(subStr,times);
+            }else {
+                map.put(subStr,map.get(subStr)+times);
+            }
+        }
+        return map;
     }
 }

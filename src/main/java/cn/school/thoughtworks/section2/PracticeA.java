da7cc3458102e584c4e5ac8121c514ecd8bbf498
wejing.qi <wenjing.qi@thoughtworks.com>
2019-03-28 16:50:13 +0800

    same elements times (should find a better way )

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,12 +1,25 @@
 package cn.school.thoughtworks.section2;
 
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
+//        java8方法
+//        return  collection1.stream().collect(Collectors.groupingBy(p -> p,Collectors.counting()));
 
-        return null;
+//        普通方法
+        Map<String,Integer> map = new HashMap<>();
+        for(int i=0;i<collection1.size();i++){
+            if(!map.containsKey(collection1.get(i))){
+                map.put(collection1.get(i),1);
+            }else {
+                map.put(collection1.get(i),map.get(collection1.get(i))+1);
+            }
+        }
+        return map;
     }
 }

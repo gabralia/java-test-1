500989f7084f7aa2334be638fa858fd82c54446d
wenjing.qi <gabralia@qq.com>
2019-03-28 23:44:13 +0800

    same elements three minus one

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -1,5 +1,6 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +8,19 @@ public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
 
-        return null;
+        Map<String,Integer> map = new HashMap<>();
+        for(String key : collectionA.keySet()) {
+            Integer number = collectionA.get(key);
+            for (String subKey : object.keySet()){
+                List<String> list = object.get(subKey);
+                if(list.contains(key)){
+                    Integer minus = number/3;
+                    map.put(key,number-minus);
+                }else {
+                    map.put(key,number);
+                }
+            }
+        }
+        return map;
     }
 }

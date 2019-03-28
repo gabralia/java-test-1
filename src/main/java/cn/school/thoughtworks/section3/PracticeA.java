72585760fcb0503bd14951cdce68e5b04e2fcb65
wenjing.qi <gabralia@qq.com>
2019-03-28 23:34:07 +0800

    same elements -1

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,5 +1,6 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +8,18 @@ public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
 
-        return null;
+        Map<String,Integer> map = new HashMap<>();
+        for(String key : collectionA.keySet()) {
+            Integer number = collectionA.get(key);
+            for (String subKey : object.keySet()){
+                List<String> list = object.get(subKey);
+                if(list.contains(key)){
+                    map.put(key,number-1);
+                }else {
+                    map.put(key,number);
+                }
+            }
+        }
+        return map;
     }
 }
